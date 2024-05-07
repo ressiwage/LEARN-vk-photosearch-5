c138d35190aff87daf918baa42a4729b0c539dad
RSWG <ressiwage@ya.ru>
2024-05-08 01:14:06 +0500

    removed my token from history

---
+++ config.py
@@ -0,0 +1,18 @@
+token = r""
+timeout = 3000.0
+headers = {
+    "accept": "*/*",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "application/x-www-form-urlencoded",
+    "origin": "https://dev.vk.com",
+    "priority": "u=1, i",
+    "referer": "https://dev.vk.com/",
+    "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+}
+interactive = True
